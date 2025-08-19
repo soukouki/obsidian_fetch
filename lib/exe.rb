bae99840b4863763f428607d2c3a88a6e5a4d883
soukouki <soukouki0@yahoo.co.jp>
2025-08-20 01:24:03 +0900

    ツールの指定方法が間違っていたのを修正

--- lib/exe.rb
+++ lib/exe.rb
@@ -3,18 +3,11 @@ require 'obsidian_fetch'
 
 # Obsidian Vaultの初期化
 vault_paths = ARGV
-vault = ObsidianFetch::Vault.new(vault_paths)
-STDERR.puts "Found #{vault.notes.size} notes"
-STDERR.puts "Found #{vault.links_by_file_name.size} links and #{vault.links_by_file_path.size} files linked by notes"
+$vault = ObsidianFetch::Vault.new(vault_paths)
+STDERR.puts "Found #{$vault.notes.size} notes"
+STDERR.puts "Found #{$vault.links_by_file_name.size} links and #{$vault.links_by_file_path.size} files linked by notes"
 
-# MCPサーバーの初期化
-server = MCP::Server.new(
-  name: "obsidian-fetch",
-  version: "0.1.0",
-  tools: []
-)
-
-# "read"ツールの定義
+# readツールの定義
 class ReadTool < MCP::Tool
   description "Read a note from Obsidian vault. If multiple notes with the same name are found, all will be shown."
   input_schema(
@@ -24,17 +17,17 @@ class ReadTool < MCP::Tool
     required: ["name"]
   )
 
-  def self.call(name:, server_context:)
+  def self.call(name:)
     # 名前が文字列でない場合
     return MCP::Tool::Response.new([{ type: "text", text: "Name must be a string" }]) unless name.is_a?(String)
 
     # Vaultからノートを読み取る
-    result = server_context[:vault].tool_read(name)
+    result = $vault.tool_read(name)
     MCP::Tool::Response.new([{ type: "text", text: result }])
   end
 end
 
-# "list"ツールの定義
+# listツールの定義
 class ListTool < MCP::Tool
   description "Search for files with matching names partially."
   input_schema(
@@ -44,21 +37,22 @@ class ListTool < MCP::Tool
     required: ["name"]
   )
 
-  def self.call(name:, server_context:)
+  def self.call(name:)
     # 名前が文字列でない場合
     return MCP::Tool::Response.new([{ type: "text", text: "Name must be a string" }]) unless name.is_a?(String)
 
     # Vaultからノートを検索
-    result = server_context[:vault].tool_list(name)
+    result = $vault.tool_list(name)
     MCP::Tool::Response.new([{ type: "text", text: result }])
   end
 end
 
-# サーバーにツールを登録
-server.tools = [ReadTool, ListTool]
-
-# サーバーコンテキストにVaultを追加
-server.server_context = { vault: vault }
+# MCPサーバーの初期化
+server = MCP::Server.new(
+  name: "obsidian-fetch",
+  version: "0.1.0",
+  tools: [ReadTool, ListTool],
+)
 
 # Stdioトランスポートを使用してサーバーを起動
 transport = MCP::Server::Transports::StdioTransport.new(server)
